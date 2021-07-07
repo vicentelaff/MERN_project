fa0030265348c1ef0c617eab25392ab9d7f0fb7b
Vicente Laffargue <vicente.laffargue@hotmail.com>
2021-07-07 18:36:09 +0200

    Backend: Handling HttpErrors (new class and model)  and new controller (places and users)

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,37 +1,13 @@
 const express = require("express");
 
+const placesController = require('../controllers/places-controller')
+
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous skyscrapers in the world!",
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516,
-    },
-    address: "20 W 34th St, New York, NY 10001",
-    creator: "u1",
-  },
-];
 
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params.pid; // { pid: 'p1' }
-  const place = DUMMY_PLACES.find((p) => {
-    return p.id === placeId;
-  });
 
-  res.json({ place }); // { place } === { palce: place }
-});
+router.get("/:pid", placesController.getPlaceById);
 
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((p) => {
-    return p.creator === userId;
-  });
-
-  res.json({ place });
-});
+router.get("/user/:uid", placesController.getPlaceByUserId);
 
 module.exports = router;
