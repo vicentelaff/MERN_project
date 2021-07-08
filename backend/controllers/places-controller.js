361986dfce1ca5abf1b4c95860aad47c61956913
Vicente Laffargue <vicente.laffargue@hotmail.com>
2021-07-08 09:08:41 +0200

    Backend: Created the update and delete place functionalities

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -1,6 +1,8 @@
+const uuid = require("uuid").v4;
+
 const HttpError = require("../models/http-error");
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
   {
     id: "p1",
     title: "Empire State Building",
@@ -24,23 +26,64 @@ const getPlaceById = (req, res, next) => {
     throw new HttpError("Could not fint a place for the provided ID", 404);
   }
 
-  res.json({ place }); // { place } === { palce: place }
+  res.json({ place }); // { place } === { place: place }
 };
 
-const getPlaceByUserId = (req, res, next) => {
+const getPlacesByUserId = (req, res, next) => {
   const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((p) => {
+  const places = DUMMY_PLACES.filter((p) => {
     return p.creator === userId;
   });
 
-  if (!place) {
+  if (!places || places.length === 0) {
     return next(
-      new HttpError("Could not fint a place for the provided ID", 404)
+      new HttpError("Could not find places for the provided user ID", 404)
     );
   }
 
-  res.json({ place });
+  res.json({ places });
+};
+
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;
+  const createdPlace = {
+    id: uuid(),
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator,
+  };
+
+  DUMMY_PLACES.push(createdPlace);
+
+  res.status(201).json({ place: createdPlace });
+};
+
+const updatePlace = (req, res, next) => {
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
+
+  // ↓ Creates a new object and copies all key/value pairs of the old object as k/v pairs in the new object.
+  const updatedPlace = { ...DUMMY_PLACES.find((p) => p.id === placeId) };
+  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
+  updatedPlace.title = title;
+  updatedPlace.description = description;
+
+  DUMMY_PLACES[placeIndex] = updatedPlace;
+
+  res.status(200).json({ place: updatedPlace });
+};
+
+const deletePlace = (req, res, next) => {
+  const placeId = req.params.pid;
+  DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
+
+  res.status(200).json({ message: "Successfully deleted place." });
 };
 
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlaceByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
+exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,13 +1,18 @@
 const express = require("express");
 
-const placesController = require('../controllers/places-controller')
+const placesController = require("../controllers/places-controller");
 
 const router = express.Router();
 
-
-
 router.get("/:pid", placesController.getPlaceById);
 
-router.get("/user/:uid", placesController.getPlaceByUserId);
+router.get("/user/:uid", placesController.getPlacesByUserId);
+
+router.post("/", placesController.createPlace);
+
+router.patch('/:pid', placesController.updatePlace)
+
+router.delete('/:pid', placesController.deletePlace)
+
 
 module.exports = router;
