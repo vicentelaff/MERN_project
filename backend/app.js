1d79454a5db9fd357ef7ee9a9be43726ef9185bc
Vicente Laffargue <vicente.laffargue@hotmail.com>
2021-07-15 15:45:20 +0200

    Added new react app frontend version

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,9 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use("/uploads/images", express.static(path.join("uploads", "images")));
+app.use(express.static(path.join("public")));
 
+// CORS error fix:
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -30,9 +32,13 @@ app.use("/api/places", placesRoutes); // => /api/places/...
 app.use("/api/users", usersRoutes);
 
 app.use((req, res, next) => {
-  const error = new HttpError("Could not find this page...", 404);
-  throw error;
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
 });
+
+// app.use((req, res, next) => {
+//   const error = new HttpError("Could not find this page...", 404);
+//   throw error;
+// });
 
 app.use((error, req, res, next) => {
   if (req.file) {
